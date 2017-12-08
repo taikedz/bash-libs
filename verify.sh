867c1181f728153a31a8c77ef35ebf6e40567d54
Tai Kedzierski <dch.tai@gmail.com>
2017-12-08 20:06:30 +0000

    add support for shellcheck

--- verify.sh
+++ verify.sh
@@ -2,12 +2,16 @@ cd "$(dirname "$0")"
 
 export BUILDOUTD=/tmp
 
+if which shellcheck >/dev/null 2>/dev/null ; then
+	flags=-c
+fi
+
 items=0
 fails=0
 
 for libscript in libs/*.sh; do
 	items=$((items+1))
-	bbuild "$libscript" || fails=$((fails+1))
+	bbuild $flags "$libscript" || fails=$((fails+1))
 done
 
 echo "Built $items items with $fails failures."
