7217581374354e5a7af73e98b1fed3c6e40b8072
Tai Kedzierski <dch.tai@gmail.com>
2017-12-28 15:25:35 +0000

    library to require variables

---
+++ libs/vars.sh
@@ -0,0 +1,40 @@
+### Vars library Usage:bbuild
+#
+# Functions for checking variables
+#
+###/doc
+
+### vars:require VARNAME ... Usage:bbuild
+#
+# Check a list of environment variables such that none are non-empty.
+#
+# If variables are empty/not set, the name is echoed.
+#
+# Returns the number of missing variables.
+#
+#	myvarA=one
+#	myvarB=
+#	myvarC=three
+#
+#	missing="$(vars:require myvarA myvarB myvarC)"
+#
+#	if [[ -n "$missing" ]]; then
+#		out:fail "Variables were not set : [$missing]"
+#	fi
+#
+###/doc
+
+vars:require() {
+	local missing=(:)
+
+	for varname in "$@"; do
+		echo "[[ -n "\$$varname" ]]" | bash || {
+			missing[${#missing[@]}]="$varname"
+		}
+	done
+
+	missing=("${missing[@]:1}")
+
+	echo "${missing[*]}"
+	return "${#missing[@]}"
+}
