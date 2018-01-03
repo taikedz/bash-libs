2799ac75beb444b9b8b599c39bd4899f22ae9f95
Tai Kedzierski <tkedzierski@optos.com>
2018-01-03 16:56:16 +0000

    Cleanup verify.sh and add README help
    Verify can now also use the bootstrap script, if supplied via the BBEXEC environment variable

--- README.md
+++ README.md
@@ -3,3 +3,36 @@
 `libs/` contains a collection of bash functions and definitions for inclusion in scripts using your bash scripts.
 
 See [`bash-builder`](https://github.com/taikedz/bash-builder) for more information.
+
+## Verifying
+
+You need to have installed bash-builder to run verification of scripts; or, run the following to use the bootstrap version
+
+	git clone https://github.com/taikedz/bash-builder
+	export BBEXEC="$PWD/bash-builder/bootstrap/bootstrap-bbuild5"
+
+To simply test the build of all libraries, run:
+
+	./verify.sh
+
+### Additional run modes
+
+You need `shellcheck` to run static analysis checks.
+
+Specify libraries explicitly:
+
+	./verify.sh libs/args.sh libs/out.sh
+
+Run corresponding tests:
+
+	dotest=y ./verify
+
+Run shellcheck after building a library (pass flags to `bbuild`):
+
+	bbflags=-c ./verify
+
+Compile the `version.sh` library, shellcheck it, and run its tests:
+
+	bbflags=-c dotest=y ./verify libs/version.sh
+
+Any combination of the above should be possible.
--- verify.sh
+++ verify.sh
@@ -6,26 +6,74 @@ export BBPATH=libs/
 items=0
 fails=0
 
-targets=(libs/*.sh)
-
-if [[ "$#" -gt 0 ]]; then
-	targets=("$@")
-fi
-
-for libscript in "${targets[@]}"; do
-	items=$((items+1))
-	bbuild "$libscript" || {
-		fails=$((fails+1))
-		continue
-	}
-
-	libname="$(basename "$libscript")"
-	if [[ -f "tests/test-$libname" ]]; then
-		bbuild "tests/test-$libname"
-		bash "/tmp/test-$libname" || fails=$((fails+1))
+set_executable() {
+	if [[ -z "${BBEXEC:-}" ]]; then
+		export BBEXEC=bbuild
 	fi
-done
 
-echo "Built $items items with $fails failures."
+	if [[ ! -f "$BBEXEC" ]] && ! which "$BBEXEC" >/dev/null 2>/dev/null; then
+		echo -e "\033[31;1mCannot use [$BBEXEC] to run builds - no such file or command\033[0m"
+		exit 1
+	fi
 
-exit "$fails"
+	echo "Build using \`$BBEXEC\` command"
+}
+
+check_runtests() {
+	runtests=no
+
+	if [[ "${dotest:-}" =~ ^(y|Y|yes|YES)$ ]]; then
+		runtests=yes
+	else
+		echo -e "\033[33;1mYou can run tests by setting the env var \`\$dotest\` to \`y\` \033[0m"
+	fi
+}
+
+set_targets() {
+	targets=(libs/*.sh)
+
+	if [[ "$#" -gt 0 ]]; then
+		targets=("$@")
+	fi
+}
+
+run_verification() {
+	for libscript in "${targets[@]}"; do
+		local scriptname="$(basename "$libscript")"
+
+		items=$((items+1))
+		"$BBEXEC" ${bbflags:-} "$libscript" || {
+			rm "/tmp/$scriptname"
+			fails=$((fails+1))
+			continue
+		}
+
+		if [[ ! "$runtests" = yes ]]; then
+			rm "/tmp/$scriptname"
+			continue
+		fi
+
+		local testname="test-$scriptname"
+		if [[ -f "tests/$testname" ]]; then
+			"$BBEXEC" "tests/$testname"
+			bash "/tmp/$testname" || fails=$((fails+1))
+			rm "/tmp/$testname"
+		else
+			echo -e "\033[33;1mWarning: there is no tests/$testname test file.\033[0m"
+		fi
+		rm "/tmp/$scriptname"
+	done
+}
+
+main() {
+	set_executable
+	check_runtests
+	set_targets "$@"
+	run_verification
+
+	echo "Built $items items with $fails failures."
+
+	exit "$fails"
+}
+
+main "$@"
