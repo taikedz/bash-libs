2db5a12b74220fe01caff48e707ecff53a48570f
Tai Kedzierski <dch.tai@gmail.com>
2018-04-04 12:07:59 +0100

    Embed abspath to tar header
    The tar header needs abspath embedded currently for paths to resolve properly

--- libs/tarheader.sh
+++ libs/tarheader.sh
@@ -13,6 +13,55 @@ set -euo pipefail
 #
 ###/doc
 
+# ===========================================================================
+##bash-libs: abspath.sh @ aa3101b7-modified
+
+### abspath:path RELATIVEPATH [ MAX ] Usage:bbuild
+# Returns the absolute path of a file/directory
+#
+# MAX defines the maximum number of "../" relative items to process
+#   default is 50
+###/doc
+
+function abspath:path {
+	local workpath="$1" ; shift || :
+	local max="${1:-50}" ; shift || :
+
+	if [[ "${workpath:0:1}" != "/" ]]; then workpath="$PWD/$workpath"; fi
+
+	workpath="$(abspath:collapse "$workpath")"
+	abspath:resolve_dotdot "$workpath" "$max" | sed -r 's|(.)/$|\1|'
+}
+
+function abspath:collapse {
+	echo "$1" | sed -r 's|/\./|/|g ; s|/\.$|| ; s|/+|/|g'
+}
+
+function abspath:resolve_dotdot {
+	local workpath="$1"; shift || :
+	local max="$1"; shift || :
+
+	# Set a limit on how many iterations to perform
+	# Only very obnoxious paths should fail
+	for x in $(seq 1 $max); do
+		# No more dot-dots - good to go
+		if [[ ! "$workpath" =~ /\.\.(/|$) ]]; then
+			echo "$workpath"
+			return 0
+		fi
+
+		# Starts with an up-one at root - unresolvable
+		if [[ "$workpath" =~ ^/\.\.(/|$) ]]; then
+			return 1
+		fi
+
+		workpath="$(echo "$workpath"|sed -r 's@[^/]+/\.\.(/|$)@@')"
+	done
+
+	# A very obnoxious path was used.
+	return 2
+}
+# ===========================================================================
 
 tarsh:unpack() {
 	trap tarsh:cleanup EXIT SIGINT
@@ -58,7 +107,7 @@ main() {
 
 	TARSH_binhash="%TARSH_ID%"
 
-	TARSH_unpackdir="$TSH_D/$(basename "$0")-$TARSH_binhash.d"
+	TARSH_unpackdir="$(abspath:path "$TSH_D/$(basename "$0")-$TARSH_binhash.d")"
 
 	tarsh:modecheck "$@"
 
