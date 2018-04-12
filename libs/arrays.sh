a4d09fdc6dbb92d3efc0b9488bada21f55039db8
Tai Kedzierski <dch.tai@gmail.com>
2018-04-12 16:39:58 +0100

    Better array library implementation

--- libs/arrays.sh
+++ libs/arrays.sh
@@ -27,70 +27,140 @@
 #  can store an array without needing to resolve splitting issues
 #
 # 	greet_all() {
-# 		local newarray=()
+# 		local sdata token
+# 		sdata=""
+#
 # 		for name in "$@"; do
-# 			newarray[${#newarray[@]}]="Hello $name"
+# 		    token=$(arrays:serialize "Hello $name")
+# 			sdata="$sdata $token"
 # 		done
-#		
-#		# Serialize the array - prints serialized data to stdout
-# 		arrays:ser "${newarray[@]}"
+#       
+#       # Specifically, do not wrap in quotes
+# 		echo $sdata
 # 	}
 #
-#	# Assigning using parentheses () splits string along whitespace to form an array
-#	#  of serialized tokens
-# 	all_greetings=( $(greet_all "Alice Alson" "Bob Robertson" "Carol Carlson") )
 #
-# 	for greeting in "${all_greetings[@]}"; do
-# 		arrays:des "$greeting"
+#   # +++++++
+# 	# Use case 1 : store serialization in a string
+#
+# 	all_greetings="$(greet_all "Alice Alson" "Bob Robertson" "Carol Carlson")"
+#
+# 	arrays:get 2 $all_greetings
+#
+#
+#   # +++++++
+#   # Use case 2 : storing the serialized tokens as an array, for iteration
+#
+#   # Assigning using parentheses () splits string along whitespace to form an array
+#   #  of serialized tokens
+#
+# 	for greeting in $all_greetings ; do    # By not quoting the string, it splits on its own
+# 		arrays:get "$greeting"
 # 	done
 #
 #
 ###/doc
 
-ARRAYSLIB_c_tb="	"
-ARRAYSLIB_c_lf="$(echo -e "\012")"
-ARRAYSLIB_c_cr="$(echo -e "\015")"
+### arrays:get { IDX TOKENS ... | TOKEN } Usage:bbuild
+#
+# Deserialize the IDX'th item in TOKENS
+#
+# or
+#
+# Deserialize TOKEN
+#
+###/doc
+arrays:get() {
+    local idx inner_array
+    idx="$1"; shift
 
-arrays:ser() {
-	arrays:serialize "$@"
+    if [[ -n "$*" ]]; then # Extract from serialized array
+        inner_array=($*)
+        [[ $idx -lt ${#inner_array[@]} ]] || return 1
+        [[ $idx -ge 0 ]] || return 1
+        arrays:deserialize "${inner_array[$idx]}"
+
+    else # No array passed; presume idx is in fact a serialized token
+        arrays:deserialize "$idx"
+    fi
 }
 
-arrays:des() {
-	arrays:deserialize "$@"
-}
+if [[ "${ARRAYSLIB_mode:-}" = escapes ]]; then
 
-arrays:serialize() {
-	local serialdata=()
-	local item
+    ### Obfuscating implementation
+    # Has some issues with newlines....
 
-	for item in "$@"; do
-		item="${item// /%s}"
-		item="${item//$ARRAYSLIB_c_tb/%t}"
-		item="${item//$ARRAYSLIB_c_lf/%n}"
-		item="${item//$ARRAYSLIB_c_cr/%r}"
+    ARRAYSLIB_c_tb="$(echo -e '\t')"
+    ARRAYSLIB_c_lf="$(echo -e "\012")"
+    ARRAYSLIB_c_cr="$(echo -e "\015")"
 
-		if [[ -z "$item" ]]; then
-			item="%0"
-		fi
+    ARRAYSLIB_s_sp="$(echo -e "\033%s")"
+    ARRAYSLIB_s_tb="$(echo -e "\033%t")"
+    ARRAYSLIB_s_lf="$(echo -e "\033%n")"
+    ARRAYSLIB_s_cr="$(echo -e "\033%r")"
+    ARRAYSLIB_s_es="$(echo -e "\033%0")"
 
-		serialdata[${#serialdata[@]}]="$item"
-	done
+    ### arrays:serialize ARGUMENTS ... Usage:bbuild
+    # 
+    # Given an array of arguments, return them as a space-separated list
+    #   of serialized tokens.
+    #
+    ###/doc
+    arrays:serialize() {
+        local serialdata item
 
-	echo "${serialdata[*]}"
-}
+        for item in "$@"; do
 
-arrays:deserialize() {
-	local item="$1"; shift
+            if [[ -z "$item" ]]; then
+                item="$ARRAYSLIB_s_es"
+            else
+                item="${item// /$ARRAYSLIB_s_sp}"
+                item="${item//$ARRAYSLIB_c_tb/$ARRAYSLIB_s_tb}"
+                item="${item//$ARRAYSLIB_c_lf/$ARRAYSLIB_s_lf}"
+                item="${item//$ARRAYSLIB_c_cr/$ARRAYSLIB_s_cr}"
+            fi
 
-	if [[ "$item" = "%0" ]]; then
-		echo ""
-		return 0
-	fi
+            serialdata="${serialdata:-} $item"
+        done
 
-	item="${item//%s/ }"
-	item="${item//%t/$ARRAYSLIB_c_tb}"
-	item="${item//%n/$ARRAYSLIB_c_lf}"
-	item="${item//%r/$ARRAYSLIB_c_cr}"
+        echo $serialdata
+    }
 
-	echo "$item"
-}
+    # Internal - see arrays:get for API use
+    #
+    # Deserialize a single token
+    arrays:deserialize() {
+        local item
+        item="$1"; shift
+
+        if [[ "$item" = "$ARRAYSLIB_s_es" ]]; then
+            echo ""
+            return 0
+        fi
+
+        item="${item//$ARRAYSLIB_s_sp/ }"
+        item="${item//$ARRAYSLIB_s_tb/$ARRAYSLIB_c_tb}"
+        item="${item//$ARRAYSLIB_s_lf/$ARRAYSLIB_c_lf}"
+        item="${item//$ARRAYSLIB_s_cr/$ARRAYSLIB_c_cr}"
+
+        echo "$item"
+    }
+
+else
+
+    arrays:serialize() {
+        local sdata x
+        sdata=''
+
+        for x in "$@"; do
+            sdata="$sdata $(echo "$x"|base64 -w0)"
+        done
+
+        echo $sdata
+    }
+
+    arrays:deserialize() {
+        echo "$1" | base64 -d
+    }
+
+fi
---
+++ tests/test-arrays.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+#%include arrays.sh test.sh
+
+AR_main=(one two "three four" five)
+
+serial_data() {
+    arrays:serialize "${AR_main[@]}"
+}
+
+test_array_extract() {
+    # TAE INDEX EXPECT ITEMS ...
+
+    local index expected serialized got
+    index="$1"; shift
+    expected="$1"; shift
+
+    serialized="$(serial_data)" # Call a function that returns serialized data
+
+    got="$(arrays:get $index "${serialized[@]}")"
+
+    echo "$got"
+    [[ "$expected" = "$got" ]]
+}
+
+test_require_idem() {
+    local item_s item_d
+
+    item_s="$(arrays:serialize "$1")"
+    item_d="$(arrays:get "$item_s")"
+
+    echo "[$1] ==> [$item_s] ==> [$item_d]"
+    [[ "$item_d" = "$1" ]]
+}
+
+test_count() {
+    local sdata i x
+    sdata="$(serial_data)"
+    sdata=($sdata) # Needed for th ${#[@]} notation below
+    i=0
+
+    for x in ${sdata[@]} ; do
+        i=$((i+1))
+    done
+
+    echo "i[$i] = ${#sdata[@]}"
+    [[ ${#sdata[@]} = $i ]]
+}
+
+### Test the alternate mode
+#ARRAYSLIB_mode=escapes
+
+test:require test_array_extract 0 one
+test:require test_array_extract 2 "three four"
+test:require test_count
+
+test:require test_require_idem "hi"
+test:require test_require_idem "you there"
+test:require test_require_idem ""
+test:require test_require_idem "$(echo -e "\t")"
+test:require test_require_idem "$(echo -e "\n")"
+test:require test_require_idem "$(echo -e "\r")"
+test:require test_require_idem "$(echo -e "\r\n")"
+test:require test_require_idem "$(echo -e "a\tb\n\r\n--")"
+
+test:require arrays:get 1 $(arrays:serialize "${AR_main[@]}")
+test:forbid arrays:get 10 $(arrays:serialize "${AR_main[@]}")
+test:forbid arrays:get -1 $(arrays:serialize "${AR_main[@]}")
+
+test:report
