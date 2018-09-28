33fa96d541fab57c659e9ac7fd814693434093ff
Tai Kedzierski <dch.tai@gmail.com>
2018-09-28 23:17:50 +0100

    make var check function introspective

--- libs/vars.sh
+++ libs/vars.sh
@@ -6,37 +6,34 @@
 #
 ###/doc
 
-### vars:require VARNAME ... Usage:bbuild
+### vars:require RETURNVAR VARNAMES ... Usage:bbuild
 #
 # Check a list of environment variables such that none are non-empty.
 #
-# If variables are empty/not set, the name is echoed.
+# If variables are empty/not set, the name is added to the return holder variable.
 #
-# Returns the number of missing variables.
+# Example:
 #
 #    myvarA=one
 #    myvarB=
 #    myvarC=three
 #
-#    missing="$(vars:require myvarA myvarB myvarC)"
-#
-#    if [[ -n "$missing" ]]; then
-#        out:fail "Variables were not set : [$missing]"
+#    if ! vars:require missing_vars myvarA myvarB myvarC ; then
+#        echo "The following variables are not set: $missing_vars"
 #    fi
 #
 ###/doc
 
 vars:require() {
-    local missing=(:)
+    local missing_var="${1:-}" ; shift || out:fail "Internal error: no aruments passed for var check"
+    local res=0
 
     for varname in "$@"; do
-        echo "[[ -n "\$$varname" ]]" | bash || {
-            missing[${#missing[@]}]="$varname"
+        . <(echo "[[ -n \"\${$varname:-}\" ]]") || {
+            . <(echo "$missing_var=\"\${$missing_var:-} $varname\"")
+            res=1
         }
     done
 
-    missing=("${missing[@]:1}")
-
-    echo "${missing[*]}"
-    return "${#missing[@]}"
+    return "$res"
 }
