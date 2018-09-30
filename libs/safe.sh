4f053f18ce489166ec4ece965037f39768f5755c
Tai Kedzierski <dch.tai@gmail.com>
2018-09-30 20:07:23 +0100

    update safe mode to provide choice of space splitting mode

--- libs/safe.sh
+++ libs/safe.sh
@@ -2,17 +2,40 @@
 
 ### Safe mode Usage:bbuild
 #
-# Set safe mode options
+# Set global safe mode options
 #
-# * Script bails on error
-# * Accessing a variable that is not set is an error
+# * Script bails if a statement or command returns non-zero status
+#   * except when in a conditional statement
+# * Accessing a variable that is not set is an error, causing non-zero status of the operation
 # * If a file glob does not expand, cause an error condition
-# * If a component of a pipe fails, the entire pipe is a failure
+# * If a component of a pipe fails, the entire pipe statement returns non-zero
+#
+# Splitting over spaces
+#
+# You can also switch space splitting on or off (normal bash default is 'on')
+#
+# Given a function `foo()` that returns multiple lines, which may each have spaces in them, use safe splitting to return each item into an array as its own item, without splitting over spaces.
+#
+#   safe:space-split off
+#   mylist=(foo)
+#   safe:space-split on
+#
+# Having space splitting on causes statements like `echo "$*"` to print each argument on its own line.
 #
 ###/doc
 
 set -eufo pipefail
 
-# TODO for future use - not to be activated until all libs have been checked
-# (all "$@" and ( <array> ) uses)
-#IFS='\t\n'
+safe:space-split() {
+    case "$1" in
+    off)
+        export IFS=$'\t\n'
+        ;;
+    on)
+        export IFS=$' \t\n'
+        ;;
+    *)
+        out:fail "API error: bad use of safe:split - must be 'on' or 'off' not '$1'"
+        ;;
+    esac
+}
