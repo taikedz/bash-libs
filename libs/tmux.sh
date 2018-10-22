d4a922f386d4e49e804eff77c5d2c5fca66ae62a
Tai Kedzierski <dch.tai@gmail.com>
2018-10-22 16:00:41 +0100

    Add tmux utility

---
+++ libs/tmux.sh
@@ -0,0 +1,88 @@
+#%include out.sh
+#%include bincheck.sh
+#%include args.sh
+
+### tmux.sh Usage:bbuild
+# 
+# Help run commands in tmux session. Useful for performing operations over remote connections.
+#       
+###/doc
+
+TMUX_ERR_in_session=1
+TMUX_ERR_run_failed=2
+
+### tmux:run COMMAND ... Usage:bbuild
+#
+# Run the command in a tmux session.
+#
+# Returns 0 if a new tmux session was created to run the script.
+#
+# Returns `$TMUX_ERR_in_session` if currently in a screen/tmux session already
+#
+# Retuns `$TMUX_ERR_run_failed` otherwise.
+#
+# Exits shell if 'tmux' is not available.
+#
+# Example:
+#
+#   main() {
+#     if tmux:run "$0" "$@"; then
+#       exit 0
+#     fi
+#
+#     apt-get update
+#     apt-get install "$@"
+#   }
+#
+#   main "$@"
+#
+###/doc
+tmux:run() {
+    tmux:available
+
+    if tmux:in-session; then
+        return $TMUX_ERR_in_session
+    fi
+
+    tmux new -d bash "$@" || return $TMUX_ERR_run_failed
+}
+
+### tmux:ensure COMMAND ... Usage:bbuild
+#
+# Run `COMMAND ...` in tmux
+#
+# Exits shell with status 0 if successfully started in tmux (do not proceed with rest of script)
+#
+# Returns 0 if already in session (proceed with rest of script)
+#
+# Exits with `$TMUX_ERR_run_failed` otherwise
+#
+# Example:
+#
+#   main() {
+#     tmux:ensure "$0" "$@"
+#
+#     apt-get update
+#     apt-get install "$@"
+#   }
+#
+#   main "$@"
+#
+###/doc
+tmux:ensure() {
+    if tmux:in-session; then
+        return 0
+    fi
+
+    if tmux:run "$@"; then
+        exit 0
+    fi
+}
+
+tmux:in-session() {
+    [[ "$TERM" = screen ]] || return $TMUX_ERR_in_session
+}
+
+tmux:available() {
+    bincheck:has tmux || out:fail "'tmux' is not available"
+}
