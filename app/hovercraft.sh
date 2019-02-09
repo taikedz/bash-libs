a43db489beb2f1643b618cc16f958da057627974
Tai Kedzierski <dch.tai@gmail.com>
2019-02-09 16:46:43 +0000

    add hovercraft application

---
+++ app/hovercraft.sh
@@ -0,0 +1,20 @@
+#%include std/syntax-extensions.sh
+#%include std/bincheck.sh
+#%include std/varify.sh
+
+### hovercraft:serve MAINFILE Usage:bashdoc
+# Build the presentation based on MAINFILE
+#
+# Opens a browser session with the presentation
+###/doc
+
+$%function hovercraft:serve(mainfile) {
+    mkdir -p presentation-hovercraft
+    local pdir="$(mktemp -d "presentation-hovercraft/$(varify:fil "$mainfile")-XXXX")"
+
+    hovercraft "$mainfile" "$pdir"
+    local runtime
+    runtime="$(bincheck:get sensible-browser firefox chromium chrome gnome-www-browser epiphany x-www-browser www-browser)" || return 1
+
+    "$runtime" "$pdir/index.html"
+}
