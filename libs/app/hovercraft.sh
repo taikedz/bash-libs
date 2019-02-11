5fbfef10f7bf487e1c703dba39e735a019ec098c
Tai Kedzierski <dch.tai@gmail.com>
2019-02-11 16:08:18 +0000

    split hovercraft serve and build options, allow override browser

--- libs/app/hovercraft.sh
+++ libs/app/hovercraft.sh
@@ -5,16 +5,35 @@
 
 #%include app/webbrowser.sh
 
-### hovercraft:serve MAINFILE Usage:bbuild
-# Build the presentation based on MAINFILE
+### hovercraft:build MAINFILE Usage:bbuild
+# Build the presentation from the MAINFILE (.rst file)
 #
-# Opens a browser session with the presentation
+# Prints the location of the presentation as a file:/// URL
 ###/doc
 
-$%function hovercraft:serve(mainfile) {
+$%function hovercraft:build(mainfile) {
     mkdir -p presentation-hovercraft
     local pdir="$(mktemp -d "presentation-hovercraft/$(varify:fil "$mainfile")-XXXX")"
 
     hovercraft "$mainfile" "$pdir"
-    webbrowser:visit "file://$PWD/$pdir/index.html"
+
+    echo "file://$PWD/$pdir/index.html"
+}
+
+### hovercraft:serve MAINFILE [BROWSER] Usage:bbuild
+# Build the presentation based on MAINFILE ;
+#
+# Opens a browser session with the presentation.
+#
+# If browser is not specified, attempts to use the default system browser.
+###/doc
+
+$%function hovercraft:serve(mainfile ?browser) {
+    local presentation="$(hovercraft:build "$mainfile")"
+
+    if [[ -z "$browser" ]]; then
+        webbrowser:visit "$presentation"
+    else
+        "$browser" "$presentation"
+    fi
 }
