7bcc05c166cc0db3648a57d68d3cf72e0f9c6fd3
Tai Kedzierski <dch.tai@gmail.com>
2019-04-03 20:57:41 +0100

    Add tests for this.sh

--- libs/std/this.sh
+++ libs/std/this.sh
@@ -12,12 +12,20 @@
 # The file name of the running script, without its path
 ###/doc
 function this:bin {
-    echo "$(basename "$0")"
+    if [[ -n "${BBRUN_SCRIPT:-}" ]]; then
+        basename "$BBRUN_SCRIPT"
+    else
+        basename "$0"
+    fi
 }
 
 ### this:bindir Usage:bbuild
 # The absolute path of the directory in which the command is running
 ###/doc
 function this:bindir {
-    echo "$(abspath:path "$(dirname "$0")" )"
+    if [[ -n "${BBRUN_SCRIPT:-}" ]]; then
+        dirname "$BBRUN_SCRIPT"
+    else
+        abspath:path "$(dirname "$0")"
+    fi
 }
--- tests/test-this.sh
+++ tests/test-this.sh
@@ -1,4 +1,40 @@
 #%include std/test.sh
 #%include std/this.sh
 
+t_source="/tmp/testing-source.sh"
+t_dest="/tmp/testing-dest.sh"
+
+
+removescripts() {
+    local s
+    for s in "$@"; do
+        [[ ! -f "$s" ]] || rm "$s"
+    done
+}
+
+equals() {
+    local desired="$1"; shift
+    local output="$("$t_dest" "$@")"
+
+    echo "$output"
+
+    [[ "$desired" = "$output" ]]
+}
+
+trap removescripts exit
+
+echo -e '#%include std/this.sh\n\n"$@"\n' > "$t_source"
+
+bbuild "$t_source" "$t_dest" || exit
+
+test:require equals testing-dest.sh this:bin
+test:require equals /tmp this:bindir
+
+# Simulate use of bbrun
+export BBRUN_SCRIPT=/var/stuff
+
+test:require equals stuff this:bin
+test:require equals /var this:bindir
+
+
 test:report
