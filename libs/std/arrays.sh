b7b73b8006700f0e4e171117b82a90b8447c0312
Tai Kedzierski <dch.tai@gmail.com>
2019-04-18 15:00:26 +0100

    Add full array deserialization to arrays.sh

--- libs/std/arrays.sh
+++ libs/std/arrays.sh
@@ -1,15 +1,18 @@
+#%include std/syntax-extensions.sh
+
 ##bash-libs: arrays.sh @ %COMMITHASH%
 
 ### Arrays Library Usage:bbuild
 #
 # Lib for handling array serialization
 #
-# !!! - consider using variable pointeres for returning arrays
-#
 # In Bash, arrays are built by splitting along whitespace.
 #  This makes it impossible for functions and commands to natively
 #  return array data whose tokens contain whitespace, or even
 #  empty-string-tokens.
+#
+# It is also impossible to pass environment variable arrays to sub
+#  processes.
 #
 # This library allows you to pass back a "serialized array."
 #
@@ -34,12 +37,14 @@
 # 	}
 #
 # 	names="$(get_names)"
+# 	# $names is a string, and can be passed down to processes.
 #
 #
 #   # +++++++
 # 	# Use case 1a : get a single item by index, and decode it
 #
 # 	arrays:get 2 $names
+#   # Not quoting the variable splits it automatically
 #
 #   # 1b : Assigning using parentheses () splits string along whitespace
 #   #  to form an array of serialized tokens. The serialized data can then
@@ -52,7 +57,7 @@
 #   # +++++++
 #   # Use case 2 : storing the serialized tokens as an array, for iteration
 #
-# 	for person in $names ; do  # Not quoting the variable splits it automatically
+# 	for person in $names ; do
 # 		arrays:get "$person"
 # 	done
 #
@@ -76,20 +81,34 @@ arrays:get() {
         inner_array=($*)
         [[ $idx -lt ${#inner_array[@]} ]] || return 1
         [[ $idx -ge 0 ]] || return 1
-        arrays:deserialize "${inner_array[$idx]}"
+        arrays:_deserialize_one "${inner_array[$idx]}"
 
     else # No array passed; presume idx is in fact a serialized token
-        arrays:deserialize "$idx"
+        arrays:_deserialize_one "$idx"
     fi
 }
 
 # Internal - see arrays:get for API use
 #
 # Deserialize a single token
-arrays:deserialize() {
+arrays:_deserialize_one() {
     echo "$1" | base64 -d
 }
 
+### arrays:deserialize ARRAYNAME TOKENS ... Usage:bbuild
+#
+# Deserialize all tokens into the referenced array.
+#
+# Note that ARRAYNAME is a string of the array name, not the
+#   array values themselves.
+#
+###/doc
+$%function arrays:deserialize(*p_return_array) {
+    local token
+    for token in "$@"; do
+        p_return_array+=("$(arrays:_deserialize_one "$token")")
+    done
+}
 
 ### arrays:serialize ARGUMENTS ... Usage:bbuild
 # 
